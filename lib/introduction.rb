1613bfe0d838d0e0ea0a216fbf69fa61e9cf1f07
Shamel Lakin <christlife4u@gmail.com>
2019-09-10 15:53:01 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+  puts "Hi I'm #{name} and I am super awesome !"
+end
+
+introduction("Shamel")
+
+def introduction_with_language(name, language)
+  puts "Hi I'm #{name} and I am super awesome at #{language} !" 
+end
+
+introduction_with_language("Shamel", "Ruby")
